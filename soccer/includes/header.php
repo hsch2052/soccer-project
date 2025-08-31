bf815df13858f5aa12a5e3219654232e432d211d
hsch2052 <165622968+hsch2052@users.noreply.github.com>
2025-08-31 20:30:23 +0900

    soccer
    .

---
+++ soccer/includes/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+<header class="header">
+    <nav class="nav">
+      <ul class="nav-left">
+        <li><a href="index.php">홈</a></li>
+        <li><a href="#">뉴스</a></li>
+        <li><a href="#">이적시장</a></li>
+      </ul>
+      <div class="logo">⚽</div>
+      <ul class="nav-right">
+        <li>
+          <div class="search-box">
+            <input type="text" placeholder="검색">
+            <button>🔍</button>
+          </div>
+        </li>
+        <li><a href="#" class="btn-signup">회원가입</a></li>
+        <li><button id="dark-mode-toggle" class="dark-toggle">🌙</button></li>
+      </ul>
+    </nav>
+
+    <script src="js/header.js"></script>
+    
+  </header>
+
+    
